48fd6687260f94d174134371218783aabee1ab28
mswift42 <martin.haesler@gmail.com>
2013-06-25 17:57:50 +0100

    Initial commit.

---
+++ gui.lisp
@@ -0,0 +1,47 @@
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "ltk")
+
+(defpackage :gui
+  (:use :cl :ltk)
+  (:export :*text-field :make-frame))
+
+(in-package :gui)
+
+
+
+(defparameter *text-field*
+  (make-instance 'text
+		 :font "monospaced" :takefocus :t))
+
+
+(defun make-frame ()
+  (with-ltk ()
+    (let* ((f (make-instance 'frame))
+	   (scroll (make-instance 'scrolled-text :master f))
+	   (outtext (textbox scroll)))
+      (pack f)
+      (configure outtext :font "monospaced" :background "#aea79f" :wrap :word)
+      (pack scroll :anchor :nw :expand t :fill :both)
+      (pack *text-field* :side :bottom :expand nil)
+      (bind *text-field* "<KeyPress-Return>"
+	    (lambda (event) (format-output outtext)))
+      (configure f :borderwidth 2))))
+
+
+(defun format-output (target)
+  "Print inputstring with newlines and > ."
+  (append-text target (format nil "~%~%> ~A~%~%" (text *text-field*)))
+  (clear-text *text-field*)
+  (print-silly-stuff target))
+
+
+(defun copy-text (target)
+  (let ((inp (text *text-field*)))
+    (format nil "~&~A" inp)
+    (format nil "~&~A" inp)))
+
+(defun print-silly-stuff (target)
+  (append-text target "A lot of silly stuff."))
+
+
+
---
+++ world.lisp
@@ -0,0 +1,135 @@
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "clunit")
+
+(defpackage :world
+  (:use :cl :clunit :gui) 
+  (:export :room :object :inventory))
+
+(in-package :world)
+
+(defclass Room ()
+  ((ldescription :initform nil :initarg :ldescription)
+   (sdescription :initform nil :initarg :sdescription)
+   (uexit :initform nil :initarg :uexit)  
+   (cexit :initform nil :initarg :cexit)  
+   (nexit :initform nil :initarg :nexit)
+   (action :initform '() :initarg :action))
+   ( :documentation "Holds long and short descriptions for locations. 
+                   Also unconditional exits from room, conditional exits from room 
+                   (for instance - a closed door, you'd have to open first) and
+                   non-exits, paths that lead nowhere but warrant a non default 
+                   explanation.")) 
+
+
+(defclass Object ()
+  ((name :initform nil :initarg :name :accessor name)
+   (synonym :initform '() :initarg :synonym :accessor :synonym)
+   (ldescription :initform '() :initarg :ldescription)
+   (sdescription :initform '() :initarg :sdescription )
+   (location :initform '() :initarg :location :accessor :location)
+   (action :initform '() :initarg :action )
+   (flags :initform '() :initarg :flags :accessor :flags))
+  (:documentation "Class for all inventory items in the game."))
+
+(defclass Inventory ()
+  ((inventory :accessor inventory :initform '() :initarg :inventory))
+  (:documentation "Players Inventory"))
+
+(defparameter *bedroom*
+  (make-instance 'Room
+		 :ldescription '(the bedroom. Very messy. Very tiny. There is a big poster
+				 on the wall. Your clothes are strewn all over the floor.
+				 Near the exit to the west is your laptop.)
+		 :sdescription '(you are in your bedroom. You should seriously think
+				 about cleaning it up.)
+		 :uexit '(( west hallway))
+		 
+		 :nexit '(east (did you seriously think about leaving by the window?
+				 I know you had a rough night but please use the door
+				 like other normal people.))))
+
+
+(defparameter *hallway*
+  (make-instance 'Room
+		 :ldescription '(the hallway. A narrow thing leading from your bedroom
+				 to the east to your frontdoor leading into town to the
+				 west.)
+		 :uexit '((east bedroom)
+			  (west frontdoor))))
+
+(defparameter *laptop*
+  (make-instance 'Object
+		 :name '(laptop)
+		 :synonym '(notebook laptop computer )
+		 :ldescription '(your old sturdy laptop. Not the latest and shiniest
+				model but money is very expensive so you still
+				 make do with it.)
+		 :sdescription '(your laptop. It used to be black.
+				 Whats the color of grime again?)
+		 :location '(*bedroom*)
+		 :action '((use-v  use-laptop) (start-v power-on-laptop) (type-pass-v crack-password-p))
+		 :flags '(poweroff )))
+
+(defun non-exits (room)
+  (first ( rest (slot-value room 'nexit))))
+
+(defun u-exits (room)
+  (slot-value room 'uexit))
+
+(defun use-laptop-f ()
+  (if (equal 'poweroff (first (:flags *laptop*)))
+      "Your laptop is turned off"
+      "you could browse your favorite websites all day, you good old procrastinator, however
+       I'd propose you simply check your Email."))
+
+(defun power-on-laptop-f ()
+  (setf (:flags *laptop*) '(poweron))
+  "You press the power button. You hear some funny noises, and it actually starts booting.
+   One Cup of Tee later, and you start at the login screen. I hope you haven't forgotten
+   the password.")
+
+
+(defun take-laptop-f ()
+  "You cannot take it. It's too heavy, the battery is not working and it's highly unlikely 
+   that it would survive any form of transport.")
+
+
+(defparameter verb-synonyms
+  '((use use-v)
+    (utilize use-v)
+    (start start-v)
+    (power start-v)))
+
+(defun return-synonym (verb)
+  (first ( rest (assoc verb verb-synonyms))))
+
+
+
+
+
+(clunit:defsuite Room-suite ())
+(clunit:defsuite Parse-suite ())
+
+(clunit:deftest test-non-exits (Room-suite)
+  (clunit:assert-equal '(did you seriously think about leaving by the window? I know you had a rough night but please use the door like other normal people.) (non-exits *bedroom*)))
+(clunit:deftest test-u-exits (Room-suite)
+  (clunit:assert-equal '((west hallway)) (u-exits *bedroom*))
+  (clunit:assert-equal '((east bedroom) (west frontdoor)) (u-exits *hallway*)))
+
+(clunit:deftest test-return-synonym (Parse-suite)
+  (clunit:assert-equal 'start-v (return-synonym 'power))
+  (clunit:assert-equal 'use-v (return-synonym 'use)))
+
+(clunit:run-suite 'Room-suite)
+(clunit:run-suite 'Parse-suite)
+
+
+
+
+
+
+
+
+
+
+
